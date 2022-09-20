f5d69f58103a356580c7f3b2fb4b6f03e0489e81
josemazhuvanchery1 <jose1998mathew@gmail.com>
2022-09-20 15:01:11 -0400

    dynamic calander

--- js/calendar.js
+++ js/calendar.js
@@ -1,9 +1,15 @@
 let nav = 0;
 let clicked = null;
+const query = name => document.querySelector(`${name}`)
 //let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
 const calendar = document.getElementById('calendar');
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+let date2 = new Date();
+const day2 = date2.getDate();
+const month2 = date2.getMonth()+1;
+const year2 = date2.getFullYear()
 
 function load() {
   const dt = new Date();
@@ -31,24 +37,57 @@ function load() {
     `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
 
   calendar.innerHTML = '';
-
+   
   for(let i = 1; i <= paddingDays + daysInMonth; i++) {
     const daySquare = document.createElement('div');
     daySquare.classList.add('day');
-
+    let divImg = document.createElement('img');
     if (i > paddingDays) {
+      let newDate = new Date(year2, month2-1+nav, i-paddingDays)
+      let newDateString = newDate.toLocaleDateString('en-us', {
+        //weekday: 'long',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+    });
+    // console.log(newDateString);
+    let toneStored = localStorage.getItem(newDateString)
+    console.log(toneStored)
+
+    if(localStorage.getItem(newDateString)){
+      //debugger
+      // let toneString = JSON.stringify(toneStored);
+      // let toneParsed = JSON.parse(toneString)
+      if(toneStored === 'positive'){
+        divImg.src = '../images/happy_face.png'
+        //debugger
+        //console.log(toneParsed)
+      }
+      else if(toneStored === 'negative'){
+        divImg.src = '../images/sad_face.png'
+        //daySquare.appendChild(divImg)
+       
+       //console.log(toneParsed)
+      }
+      else{
+        divImg.src ='../images/neutral_face.png';
+       //console.log(toneParsed)
+      }
+
+    }
       daySquare.innerText = i - paddingDays;
 
       if (i - paddingDays === day && nav === 0) {
         daySquare.id = 'currentDay';
       }
 
-      daySquare.addEventListener('click', () => console.log('clicked'));
+      daySquare.addEventListener('click', () => console.log(toneStored.split(',')[1]));
     } else {
       daySquare.classList.add('padding');
     }
 
     calendar.appendChild(daySquare);    
+    daySquare.appendChild(divImg)
   }
 }
 
--- js/sentiment.js
+++ js/sentiment.js
@@ -2,55 +2,53 @@ console.log('hello world')
 let textValue = document.getElementById('text')
 const submitBttn = document.getElementById('submit');
 submitBttn.addEventListener('click', getSentiment)
- 
-async function getSentiment(){
-const data = textValue.value;
-const encodedParams = new URLSearchParams();
-encodedParams.append("text", data);
 
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/x-www-form-urlencoded',
-		'X-RapidAPI-Key': 'c936b82e70msh983293dad2515dap15069ajsncaf8ff6e293c',
-		'X-RapidAPI-Host': 'twinword-sentiment-analysis.p.rapidapi.com'
-	},
-	body: encodedParams
-};
+async function getSentiment() {
+    const data = textValue.value;
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("text", data);
 
-let fetchedData = await fetch('https://twinword-sentiment-analysis.p.rapidapi.com/analyze/', options)
-	.then(response =>response.json())
-	.catch(err => console.error(err));
-const score = fetchedData.score;
-const tone = fetchedData.type;
-console.log(fetchedData)
-let imgDiv = document.getElementById('emoji');
-if(fetchedData.type === 'positive'){
-    imgDiv.src = '../images/happy_face.png'
-}
-else if(fetchedData.type === 'negative'){
-    imgDiv.src = '../images/sad_face.png'
-}
-else imgDiv.src = '../images/neutral_face.png';
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Key': 'c936b82e70msh983293dad2515dap15069ajsncaf8ff6e293c',
+            'X-RapidAPI-Host': 'twinword-sentiment-analysis.p.rapidapi.com'
+        },
+        body: encodedParams
+    };
 
+    let fetchedData = await fetch('https://twinword-sentiment-analysis.p.rapidapi.com/analyze/', options)
+        .then(response => response.json())
+        .catch(err => console.error(err));
+    const score = fetchedData.score;
+    const tone = fetchedData.type;
+    console.log(fetchedData)
+    let imgDiv = document.getElementById('emoji');
+    if (fetchedData.type === 'positive') {
+        imgDiv.src = '../images/happy_face.png'
+    }
+    else if (fetchedData.type === 'negative') {
+        imgDiv.src = '../images/sad_face.png'
+    }
+    else imgDiv.src = '../images/neutral_face.png';
 
-}
-
-let dateBttn = document.getElementById('enterDate')
-dateBttn.addEventListener('click', ()=>{
-    let date = new Date(document.getElementById( 'dateEntry' ).value);
+    let date = new Date(document.getElementById('dateEntry').value);
     //date = new Date (date.toUTCString())
     //let d = new Date( input );
     let dateString2 = date.toLocaleDateString('en-us', {
-        weekday: 'long',
+        //weekday: 'long',
         year: 'numeric',
         month: 'numeric',
         day: 'numeric',
-      });
-      console.log(dateString2)
-    console.log('year', date.getFullYear())
-console.log('month', date.getMonth()+1)
-console.log('day', date.getDate())
+    });
+    // console.log(dateString2)
+    let obj = {toneVal: tone,
+        dataVal:data
+    }
+    // let objParsed = JSON.parse(JSON.stringify(obj))
+    // console.log(objParsed)
+    localStorage.setItem(dateString2,[tone, data]);
+}
 
-})
 
